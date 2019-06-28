b14f8ed90c142d5a99566572992c39454f8896b6
Aliaskar <aliaskar.kom.07@gmail.com>
2019-06-28 11:07:48 +0600

    testing

---
+++ pick_a_card.py
@@ -0,0 +1,11 @@
+import random
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suits = ['Diamonds','Hearts','Clubs','Spades']#Буби , Черви, Крести,Пики
+puts =values + suits
+print(puts)
+
+
+random_point = random.choice(values)
+random_sing = random.choice(suits)
+random_card = random_point,random_sing
+print(random_card)
